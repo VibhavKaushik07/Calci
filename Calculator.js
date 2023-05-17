bf6e26dfadae87668f1cf62ffe049d2ad3d5c986
Blaze2002007 <133836546+Blaze2002007@users.noreply.github.com>
2023-05-17 09:00:22 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,15 @@
+let input = document.getElementById("input");
+
+function addToInput(val) {
+  input.value += val;
+}
+
+function clearInput() {
+  input.value = "";
+}
+
+function calculate() {
+  let equation = input.value;
+  let result = eval(equation);
+  input.value = result;
+}
